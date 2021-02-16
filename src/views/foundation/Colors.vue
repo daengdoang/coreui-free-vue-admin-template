cc65f3e6eb69184e9718fa4dc008c478239033aa
Daeng Muhammad Feisal <daengdoang@gmail.com>
2021-02-16 20:07:38 +0700

    add primary and secondary colors, also shades and tint of cashbac blue and black

--- src/views/foundation/Colors.vue
+++ src/views/foundation/Colors.vue
@@ -2,7 +2,68 @@
   <div class="animated fadeIn">
     <b-card header-tag="header">
       <div slot="header">
-      Primary Color (Cashbac Blue)
+      Primary Color
+      </div>
+      <b-card-body>
+        <div style="background:#26ade4; width:180px; height:208px; display:inline-block; margin: 0 12px; position:relative; z-index:0; box-shadow: 0 2px 4px rgb(0 0 0 / 10%); " class="color-card" >
+          <div style="position: absolute; width: 100%; bottom: 8px; background:#fff; padding: 16px; line-height:0;" class="color-card__info" >
+            <h3 style="font-size: 14pt;" >CASHBAC50</h3>
+            <p>HEX: #26ADE4</p>
+            
+          </div>
+        </div>
+        <div style="background:#4dbd74; width:180px; height:208px; display:inline-block; margin: 0 12px; position:relative; z-index:0; box-shadow: 0 2px 4px rgb(0 0 0 / 10%); " class="color-card" >
+          <div style="position: absolute; width: 100%; bottom: 8px; background:#fff; padding: 16px; line-height:0;" class="color-card__info" >
+            <h3 style="font-size: 14pt;" >GREEN50</h3>
+            <p>HEX: #4dbd74</p>
+            
+          </div>
+        </div>
+        <div style="background:#1F2D3E; width:180px; height:208px; display:inline-block; margin: 0 12px; position:relative; z-index:0; box-shadow: 0 2px 4px rgb(0 0 0 / 10%); " class="color-card" >
+          <div style="position: absolute; width: 100%; bottom: 8px; background:#fff; padding: 16px; line-height:0;" class="color-card__info" >
+            <h3 style="font-size: 14pt;" >BLACK50</h3>
+            <p>HEX: #1F2D3E</p>
+            
+          </div>
+        </div>
+      </b-card-body>
+    </b-card>
+    <b-card header-tag="header">
+      <div slot="header">
+      Secondary Color
+      </div>
+      <b-card-body>
+        <div style="background:#4dbd74; width:180px; height:208px; display:inline-block; margin: 0 12px; position:relative; z-index:0; box-shadow: 0 2px 4px rgb(0 0 0 / 10%); " class="color-card" >
+          <div style="position: absolute; width: 100%; bottom: 8px; background:#fff; padding: 16px; line-height:0;" class="color-card__info" >
+            <h3 style="font-size: 14pt;" >GREEN50</h3>
+            <p>HEX: #4dbd74</p>            
+          </div>
+        </div>
+        <div style="background:#f86c6b; width:180px; height:208px; display:inline-block; margin: 0 12px; position:relative; z-index:0; box-shadow: 0 2px 4px rgb(0 0 0 / 10%); " class="color-card" >
+          <div style="position: absolute; width: 100%; bottom: 8px; background:#fff; padding: 16px; line-height:0;" class="color-card__info" >
+            <h3 style="font-size: 14pt;" >RED50</h3>
+            <p>HEX: #f86c6b</p>            
+          </div>
+        </div>
+        <div style="background:#ffc107; width:180px; height:208px; display:inline-block; margin: 0 12px; position:relative; z-index:0; box-shadow: 0 2px 4px rgb(0 0 0 / 10%); " class="color-card" >
+          <div style="position: absolute; width: 100%; bottom: 8px; background:#fff; padding: 16px; line-height:0;" class="color-card__info" >
+            <h3 style="font-size: 14pt;" >YELLOW50</h3>
+            <p>HEX: #ffc107</p>            
+          </div>
+        </div>
+        <div style="background:#63c2de; width:180px; height:208px; display:inline-block; margin: 0 12px; position:relative; z-index:0; box-shadow: 0 2px 4px rgb(0 0 0 / 10%); " class="color-card" >
+          <div style="position: absolute; width: 100%; bottom: 8px; background:#fff; padding: 16px; line-height:0;" class="color-card__info" >
+            <h3 style="font-size: 14pt;" >INFO50</h3>
+            <p>HEX: #63c2de</p>            
+          </div>
+        </div>
+
+      </b-card-body>
+    </b-card>
+
+    <b-card header-tag="header">
+      <div slot="header">
+      Cashbac Blue - Shades and Tints
       </div>
       <b-card-body>
         <div style="background:#E9F6FC; width:180px; height:208px; display:inline-block; position:relative; z-index:0; box-shadow: 0 2px 4px rgb(0 0 0 / 10%); " class="color-card" >
@@ -37,6 +98,49 @@
           <div style="position: absolute; width: 100%; bottom: 8px; background:#fff; padding: 16px; line-height:0;" class="color-card__info" >
             <h3 style="font-size: 14pt;" >CASHBAC80</h3>
             <p>HEX: #166788</p>
+            
+          </div>
+        </div>
+      </b-card-body>
+    </b-card>
+
+    <b-card header-tag="header">
+      <div slot="header">
+      BLACK - Shades and Tints
+      </div>
+      <b-card-body>
+        <div style="background:#E9EAEC; width:180px; height:208px; display:inline-block; position:relative; z-index:0; box-shadow: 0 2px 4px rgb(0 0 0 / 10%); " class="color-card" >
+          <div style="position: absolute; width: 100%; bottom: 8px; background:#fff; padding: 16px; line-height:0;" class="color-card__info" >
+            <h3 style="font-size: 14pt;" >BLACK10</h3>
+            <p>HEX: #E9EAEC</p>
+            
+          </div>
+        </div>
+        <div style="background:#4B5764; width:180px; height:208px; display:inline-block; position:relative; z-index:0; box-shadow: 0 2px 4px rgb(0 0 0 / 10%); " class="color-card" >
+          <div style="position: absolute; width: 100%; bottom: 8px; background:#fff; padding: 16px; line-height:0;" class="color-card__info" >
+            <h3 style="font-size: 14pt;" >BLACK40</h3>
+            <p>HEX: #4B5764</p>
+            
+          </div>
+        </div>
+        <div style="background:#1F2D3E; width:180px; height:208px; display:inline-block; position:relative; z-index:0; box-shadow: 0 2px 4px rgb(0 0 0 / 10%); " class="color-card" >
+          <div style="position: absolute; width: 100%; bottom: 8px; background:#fff; padding: 16px; line-height:0;" class="color-card__info" >
+            <h3 style="font-size: 14pt;" >BLACK50</h3>
+            <p>HEX: #1F2D3E</p>
+            
+          </div>
+        </div>
+        <div style="background:#182431; width:180px; height:208px; display:inline-block; position:relative; z-index:0; box-shadow: 0 2px 4px rgb(0 0 0 / 10%); " class="color-card" >
+          <div style="position: absolute; width: 100%; bottom: 8px; background:#fff; padding: 16px; line-height:0;" class="color-card__info" >
+            <h3 style="font-size: 14pt;" >BLACK60</h3>
+            <p>HEX: #182431</p>
+            
+          </div>
+        </div>
+        <div style="background:#0C1218; width:180px; height:208px; display:inline-block; position:relative; z-index:0; box-shadow: 0 2px 4px rgb(0 0 0 / 10%); " class="color-card" >
+          <div style="position: absolute; width: 100%; bottom: 8px; background:#fff; padding: 16px; line-height:0;" class="color-card__info" >
+            <h3 style="font-size: 14pt;" >BLACK80</h3>
+            <p>HEX: #0C1218</p>
             
           </div>
         </div>
