de1b74f6e459018b4377ccfcabbb7800f9092f99
Daeng Muhammad Feisal <daengdoang@gmail.com>
2021-02-17 16:13:20 +0700

    Update Credit.vue

--- src/views/Credit.vue
+++ src/views/Credit.vue
@@ -13,7 +13,7 @@
           Credit
         </template>
         <template slot="lead">
-          This page deicated as hall of fame from Cashbac Web UI Kit
+          This page dedicated as hall of fame from Cashbac Web UI Kit
         </template>
         <hr class="my-4">
       </b-jumbotron>
@@ -25,12 +25,12 @@
         <b-card>
           <i class="icon-chart icons font-2xl d-block mt-4"></i>
           <p> </p>
-          <h3>Cashbac Ops Dashboard (Settlement and Promo) Squad</h3>
+          <h4>Cashbac Ops Dashboard Squad</h4>
           <br />
           <ul>
-            <li>Daeng Muhammad Feisal <strong>(UI/UX Designer)</strong</li>
-            <li>Ignatius Oktaviardi <strong>(Front End Developer)</strong</li>
-            <li>Miftah Ramadhan <strong>(QA Engineer)</strong</li>
+            <li>Daeng Muhammad Feisal <strong>(UI/UX Designer)</strong></li>
+            <li>Ignatius Oktaviardi <strong>(Front End Developer)</strong></li>
+            <li>Miftah Ramadhan <strong>(QA Engineer)</strong></li>
             <li>Hilmiatun Nisa <strong>(Product Manager)</strong></li>
           </ul>
           <p> </p>
@@ -38,31 +38,49 @@
         <b-card >
           <i class="icon-chart icons font-2xl d-block mt-4"></i>
           <p> </p>
-          <h3>Cashbac Merchant Dashboard Squad</h3>
+          <h4>Cashbac Merchant Dashboard Squad</h4>
           <br />
           <ul>
             <li>Daeng Muhammad Feisal <strong>(UI/UX Designer)</strong</li>
-            <li>Cristyna Tan <strong>(UI/UX Designer and Illustrator)</strong</li>
-            <li>Rafiandra Widhiansyah <strong>(UI/UX Designer)</strong</li>
-            <li>Robby Hermansyah <strong>(Front End Developer)</strong</li>
-            <li>Andre "Gever" Pratama <strong>(Product Manager)</strong></li>
+            <li>Cristyna Tan <strong>(UI/UX Designer and Illustrator)</strong></li>
+            <li>Rafiandra Widhiansyah <strong>(UI/UX Designer)</strong></li>
+            <li>Ghea Melati <strong>(UX Writer)</strong></li>
+            <li>Robby Hermansyah <strong>(Front End Developer)</strong></li>
+            <li>Panca Ramadhanty <strong>(QA Engineer)</strong</li>
+            <li>Andrea "Gever" Pratama <strong>(Product Manager)</strong></li>
           </ul>          
           <p></p>  
         </b-card>
         <b-card >
           <i class="icon-globe icons font-2xl d-block mt-4"></i>
           <p> </p>
-          <h3>Cashbac Landing Page Squad</h3>
+          <h4>Cashbac Landing Page Squad</h4>
           <br />
           <ul>
-            <li>Daeng Muhammad Feisal <strong>(UI/UX Designer)</strong</li>
-            <li>Febriana Afra Hanifah <strong>(Content Writer)</strong</li>
-            <li>Mary Chelsea <strong>(Marketing)</strong</li>
-            <li>Jepri Sinaga <strong>(Product Manager)</strong></li>
+            <li>Daeng Muhammad Feisal <strong>(UI/UX Designer & Front-End Developer)</strong></li>
+            <li>Jepri Sinaga <strong>(Front-End Developer / Product Manager)</strong></li>
+            <li>Mary Chelsea <strong>(Copywriter Marketing)</strong></li>
+            <li>Febriana Afra Hanifah <strong>(Content Writer)</strong></li>
+            <li>Windi Romania <strong>(Illustrator / UX Lead)</strong></li>
+
+          </ul>   
+          <p></p>
+        </b-card>
+                <b-card >
+          <i class="icon-envelope-letter icons font-2xl d-block mt-4"></i>
+          <p> </p>
+          <h4>Template Email Redesign</h4>
+          <br />
+          <ul>
+            <li>Daeng Muhammad Feisal <strong>(UI/UX Designer & Front-End Developer)</strong></li>
+            <li>Hengki Sihombing <strong>(Front-End Developer / VP Product)</strong></li>
+            <li>Lilis Suryadi <strong>(QA Engineer)</strong></li>
+            <li>Yudha Afriyanto <strong>(Lead QA Engineer)</strong></li>
           </ul>   
           <p></p>
         </b-card>
     </b-card-group>
+    <br />
     <br />
   </div>
 </template>
